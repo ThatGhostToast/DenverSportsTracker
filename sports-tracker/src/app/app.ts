ec089f81596b358c405757fbc8e0f9942d9d124c
Zac Almas <zacalmas@gmail.com>
2026-01-07 14:40:21 -0500

    Structure Commit
    Adding the structure of the application

---
+++ sports-tracker/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardComponent} from '../components/card/card.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CardComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('sports-tracker');
+}
