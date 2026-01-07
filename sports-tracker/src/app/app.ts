445eb416f4a2e605f53ac7ee00ec548fddd80bc3
Zac Almas <zacalmas@gmail.com>
2026-01-07 14:53:23 -0500

    Copilot Suggestions
    Implementing the copilot suggestions

--- sports-tracker/src/app/app.ts
+++ sports-tracker/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CardComponent} from '../components/card/card.component';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CardComponent } from '../components/card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent],
+  imports: [CardComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('sports-tracker');
--- sports-tracker/src/components/card/card.component.ts
+++ sports-tracker/src/components/card/card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import {ChangeDetectionStrategy, Component} from "@angular/core";
 
 @Component({
   selector: "app-card",
   standalone: true,
   templateUrl: "./card.component.html",
-  styleUrl: "./card.component.css"
+  styleUrl: "./card.component.css",
+  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class CardComponent {}
