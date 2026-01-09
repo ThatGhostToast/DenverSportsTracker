7f5d75bf04665474abd004bfb1457a3723349794
Zac Almas <zacalmas@gmail.com>
2026-01-08 19:25:41 -0500

    Updates
    Updating the css styles in order to make the cards look more like I wanted.
    
    Added test data to play around with

--- sports-tracker/src/components/card/card.component.ts
+++ sports-tracker/src/components/card/card.component.ts
@@ -8,4 +8,22 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class CardComponent {}
+export class CardComponent {
+
+  teamData: any = [{
+      teamName: "Denver Broncos",
+      teamLogo: "https://upload.wikimedia.org/wikipedia/en/4/44/Denver_Broncos_logo.svg",
+      recentScore: "3 - 19"
+    },
+    {
+      teamName: "Colorado Avalanche",
+      teamLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Colorado_Avalanche_logo.svg/2560px-Colorado_Avalanche_logo.svg.png",
+      recentScore: "2 - 4"
+    },
+    {
+      teamName: "Denver Nuggets",
+      teamLogo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Denver_Nuggets.svg/2560px-Denver_Nuggets.svg.png",
+      recentScore: "114 - 110"
+    }
+  ];
+}
